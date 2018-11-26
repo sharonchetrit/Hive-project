929f10db0359c9c64ead494e7d28f15dae1457a3
zribi <raphael@MacBook-Pro-de-zribi.local>
2018-11-26 09:55:53 +0200

    added populate + tweet class -> post class + settingss fix error + navbar finish (templates-> base.html)

--- hive_project/hive_project/settings.py
+++ hive_project/hive_project/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 SITE_ROOT  = os.path.dirname(os.path.realpath(__file__))
 STATIC_DIR = os.path.join(SITE_ROOT, 'static/')
 
@@ -121,6 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    STATIC_DIR,
+STATICFILES_DIRS = [
+    STATIC_DIR
 ]
