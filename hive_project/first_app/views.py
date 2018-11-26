f221dbdc6fa733cf31df992fdc69f3a24959ab86
zribi <raphael@MacBook-Pro-de-zribi.local>
2018-11-26 11:52:24 +0200

    homepage user feed

--- hive_project/first_app/views.py
+++ hive_project/first_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from first_app.models import UserProfile, Post
+#from . import forms
+import datetime
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'index.html')
+	posts = Post.objects.all().order_by('-date')[:30]
+	return render(request, 'index.html', { 'posts': posts })
 
 def signup(request):
 	return render(request, 'signup.html') 
+
 def login(request):
 	return render(request, 'login.html')
